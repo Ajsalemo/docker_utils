aba1fad2926fc6537e6734f8c774c69f6f1528d5
Anthony Salemo <ajssalemo@gmail.com>
2021-01-12 16:53:04 -0500

    able to alias the script and properly push to acr

--- docker_make_utils.sh
+++ docker_make_utils.sh
@@ -2,7 +2,7 @@
 
 IMAGE_NAME=$1
 TAG_NAME=$2
-ACR_NAME=$ACR_NAME
+ACR_NAME="ansalemocontainerregistry.azurecr.io"
 
 show_help() {
     printf "Usage: <command> options [parameters]\n"
@@ -23,16 +23,20 @@ throw_error() {
 run_acr_build() {
     # If an argument for 'TAG_NAME' is not provided then build the image with the 'latest' tag
     if [[ "$IMAGE_NAME" != "" && "$TAG_NAME" == "" ]]; then
-        echo "Building image with image name: '$IMAGE_NAME' and tag 'latest'."
-        docker build -t $IMAGE_NAME .
-        echo "Tagging image '$IMAGE_NAME' with tag 'latest'"
-        docker tag $IMAGE_NAME $ACR_NAME/$IMAGE_NAME
+            echo "Building image with image name: '$IMAGE_NAME' and tag 'latest'."
+            docker build -t $IMAGE_NAME .
+            echo "Tagging image '$IMAGE_NAME' with tag 'latest'"
+            docker tag $IMAGE_NAME $ACR_NAME/$IMAGE_NAME
+            echo "Pushing image '$IMAGE_NAME' with tag 'latest' to Azure Container Registry."
+            docker push $ACR_NAME/$IMAGE_NAME:latest
     # If an argument for 'TAG_NAME' is provided, build and tag the image with the provided tag name
     elif [[ "$IMAGE_NAME" != "" && "$TAG_NAME" != "" ]]; then
-        echo "Building image with image name: '$IMAGE_NAME' and tag '$TAG_NAME'"
-        docker build -t $IMAGE_NAME .
-        echo "Tagging image '$IMAGE_NAME' with tag '$TAG_NAME'"
-        docker tag $IMAGE_NAME $ACR_NAME/$IMAGE_NAME:$TAG_NAME 
+            echo "Building image with image name: '$IMAGE_NAME' and tag '$TAG_NAME'"
+            docker build -t $IMAGE_NAME .
+            echo "Tagging image '$IMAGE_NAME' with tag '$TAG_NAME'"
+            docker tag $IMAGE_NAME $ACR_NAME/$IMAGE_NAME:$TAG_NAME
+            echo "Pushing image '$IMAGE_NAME' with tag '$TAG_NAME' to Azure Container Registry."
+            docker push $ACR_NAME/$IMAGE_NAME:$TAG_NAME
     # Catch errors
     else
         throw_error
@@ -43,8 +47,8 @@ if [[ "$IMAGE_NAME" == "-h" || "$IMAGE_NAME" == "--help" ]] || [[ "$TAG_NAME" ==
     show_help
 # If the image name argument is then throw an error
 # If the arguments provided are 0 OR are greater than 2 throw an error
-elif [[ "$IMAGE_NAME" == "" ]] ||  [[ $# -gt 2 || $# -eq 0 ]]; then
+elif [[ "$IMAGE_NAME" == "" ]] || [[ $# -gt 2 || $# -eq 0 ]]; then
     throw_error
-else 
+else
     run_acr_build
 fi
